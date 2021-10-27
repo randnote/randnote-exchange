dac6fb33e65ef5dd7ba7e3e8ad77e89416cd07e4
danielromeo <danielromeo99@gmail.com>
2021-10-27 22:05:42 +0200

    changed verified email from string to boolean

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
