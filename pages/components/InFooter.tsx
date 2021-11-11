45d04ac0d2e4b5015f6bbd324a5aa29b94957791
danielromeo <danielromeo99@gmail.com>
2021-11-11 14:42:16 +0200

    previously uncommited files

--- pages/components/InFooter.tsx
+++ pages/components/InFooter.tsx
@@ -1,13 +1,15 @@
 import type { NextPage } from "next";
-import styles from "./styles/infooter.module.css";
+import styles from "./styles/infooter.module.css"
 
-const footerClassName = `${styles.footer} page-footer font-small blue pt-4 `;
+const footerClassName = `${styles.footer} page-footer font-small blue pt-4 `
 
 const InFooter = () => {
 	return (
 		<footer className={footerClassName}>
 			<div className="container-fluid text-center text-md-left">
-				<div className="row">something maybe here</div>
+				<div className="row">
+					something maybe here
+				</div>
 			</div>
 			<div className="footer-copyright text-center py-3">
 				© 2020 Copyright RANDNOTE(Open source):
@@ -15,12 +17,5 @@ const InFooter = () => {
 		</footer>
 	);
 };
-
-// const styles: object = {
-//     position: "fixed",
-//     height: "100px",
-//     bottom: 0,
-//     width: "100%"
-// }
 
 export default InFooter;
