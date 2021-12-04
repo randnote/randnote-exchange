333c107ad5a93473ddf0615f0d74214e815d99ff
danielromeo <danielromeo99@gmail.com>
2021-12-04 20:44:34 +0200

    styling the buttons in the navbar

--- pages/components/Navbar.tsx
+++ pages/components/Navbar.tsx
@@ -75,4 +75,70 @@ const MainNavbar = (props: any) => {
 		</div>
 	);
 };
+
+
+export const AuthenticatedNavbar = (props: any) => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const toggle = () => setIsOpen(!isOpen);
+
+	return (
+		<div>
+			<Navbar style={AuthNavStyles} expand="md">
+				<NavbarBrand href="/">reactstrap</NavbarBrand>
+				<NavbarToggler onClick={toggle} />
+				<Collapse isOpen={isOpen} navbar>
+					<Nav className="mr-auto" navbar>
+						<NavItem>
+							<NavLink style={{color: "white"}} href="/components/">Components</NavLink>
+						</NavItem>
+
+						<NavItem>
+							<NavLink href="https://github.com/reactstrap/reactstrap">
+								GitHub
+							</NavLink>
+						</NavItem>
+						<UncontrolledDropdown nav inNavbar>
+							<DropdownToggle nav caret>
+								Options
+							</DropdownToggle>
+							<DropdownMenu right>
+								<DropdownItem>Option 1</DropdownItem>
+								<DropdownItem>Option 2</DropdownItem>
+								<DropdownItem divider />
+								<DropdownItem>Reset</DropdownItem>
+							</DropdownMenu>
+						</UncontrolledDropdown>
+					</Nav>
+
+					<Nav className="navbar-nav ms-auto">
+						<NavItem  className="ms-auto">
+							
+								<Link  href="signin">
+									<button class="btn " styles={AuthNavbarButtonStyles} color="" >SignIn</button>
+								</Link>
+							
+							<Link href="signup">
+									<Button styles={AuthNavbarButtonStyles} color="primary">SignUp</Button>
+								</Link>
+						</NavItem>
+					</Nav>
+				</Collapse>
+			</Navbar>
+		</div>
+	);
+};
+
+const AuthNavStyles = {
+	backgroundColor: "green"
+}
+
+const AuthNavbarButtonStyles = {
+	color: "red",
+	margin: '5px',
+	borderRadius: '10px'
+}
+
+
+
 export default MainNavbar;
--- pages/deposit.tsx
+++ pages/deposit.tsx
@@ -1,99 +1,97 @@
 import type { NextPage } from "next";
-import { Container, Button , Input, Row, Col} from "reactstrap";
-import MainNavbar from "./components/Navbar";
- import 'react-credit-cards/es/styles-compiled.css';
+import { Container, Button, Input, Row, Col } from "reactstrap";
+import MainNavbar,{AuthenticatedNavbar} from "./components/Navbar";
+import "react-credit-cards/es/styles-compiled.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import React from 'react';
-import Cards from 'react-credit-cards';
- 
+import React from "react";
+import Cards from "react-credit-cards";
+
 class PaymentForm extends React.Component {
-  state = {
-    cvc: '',
-    expiry: '',
-    focus: '',
-    name: '',
-    number: '',
-  };
- 
-  handleInputFocus = (e) => {
-    this.setState({ focus: e.target.name });
-  }
-  
-  handleInputChange = (e) => {
-    const { name, value } = e.target;
-    
-    this.setState({ [name]: value });
-  }
-  
-  render() {
-    return (
-      <div id="PaymentForm">
-      <Container>
-      <Row>
-      <Col md='6'>
-        <Cards
-          cvc={this.state.cvc}
-          expiry={this.state.expiry}
-          focused={this.state.focus}
-          name={this.state.name}
-          number={this.state.number}
-        />
+	state = {
+		cvc: "",
+		expiry: "",
+		focus: "",
+		name: "",
+		number: "",
+	};
 
-        
-        </Col>
+	handleInputFocus = (e) => {
+		this.setState({ focus: e.target.name });
+	};
 
-        <Col md='6'>
-        	<form>
-        	<Input
-            type="tel"
-            name="number"
-            placeholder="Card Number"
-            onChange={this.handleInputChange}
-            onFocus={this.handleInputFocus}
-          />
-        
-        	<Input
-            type="tel"
-            name="name"
-            placeholder="Name as displayed on your card"
-            onChange={this.handleInputChange}
-            onFocus={this.handleInputFocus}
-          />
-          
-          
-        	<Input
-            type="tel"
-            name="expiry"
-            placeholder=""
-            onChange={this.handleInputChange}
-            onFocus={this.handleInputFocus}
-          />
+	handleInputChange = (e) => {
+		const { name, value } = e.target;
 
-        	<Input
-            type="tel"
-            name="cvc"
-            placeholder="cvc"
-            onChange={this.handleInputChange}
-            onFocus={this.handleInputFocus}
-          />
+		this.setState({ [name]: value });
+	};
 
-        </form>
-        </Col>
-        </Row>
-        </Container>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div id="PaymentForm">
+				<AuthenticatedNavbar></AuthenticatedNavbar>
+
+				<Container>
+					<Row>
+						<Col md="6">
+							<Cards
+								cvc={this.state.cvc}
+								expiry={this.state.expiry}
+								focused={this.state.focus}
+								name={this.state.name}
+								number={this.state.number}
+							/>
+						</Col>
+
+						<Col md="6">
+							<form>
+								<Input
+									type="tel"
+									name="number"
+									placeholder="Card Number"
+									onChange={this.handleInputChange}
+									onFocus={this.handleInputFocus}
+								/>
+
+								<Input
+									type="tel"
+									name="name"
+									placeholder="Name as displayed on your card"
+									onChange={this.handleInputChange}
+									onFocus={this.handleInputFocus}
+								/>
+
+								<Input
+									type="tel"
+									name="expiry"
+									placeholder=""
+									onChange={this.handleInputChange}
+									onFocus={this.handleInputFocus}
+								/>
+
+								<Input
+									type="tel"
+									name="cvc"
+									placeholder="cvc"
+									onChange={this.handleInputChange}
+									onFocus={this.handleInputFocus}
+								/>
+							</form>
+						</Col>
+					</Row>
+				</Container>
+			</div>
+		);
+	}
 }
 
 const Deposit: NextPage = () => {
 	return (
 		<div>
-			<Container>
-			Deposit
+			
+				
 				<PaymentForm></PaymentForm>
-			</Container>
+			
 		</div>
 	);
 };
