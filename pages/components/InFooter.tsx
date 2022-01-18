ab23c7eec738e73e0ac0ff955d28282abbf30ebd
danielromeo <danielromeo99@gmail.com>
2022-01-18 15:18:03 +0200

    testing commit

--- pages/components/InFooter.tsx
+++ pages/components/InFooter.tsx
@@ -1,15 +1,12 @@
-import type { NextPage } from "next";
-import styles from "./styles/infooter.module.css"
+import styles from "./styles/infooter.module.css";
 
-const footerClassName = `${styles.footer} page-footer font-small blue pt-4 `
+const footerClassName = `${styles.footer} page-footer font-small blue pt-4 `;
 
 const InFooter = () => {
 	return (
 		<footer className={footerClassName}>
 			<div className="container-fluid text-center text-md-left">
-				<div className="row">
-					something maybe here
-				</div>
+				<div className="row">something maybe here</div>
 			</div>
 			<div className="footer-copyright text-center py-3">
 				© 2020 Copyright RANDNOTE(Open source):
--- pages/deposit.tsx
+++ pages/deposit.tsx
@@ -1,11 +1,15 @@
 import type { NextPage } from "next";
 import { Container, Button } from "reactstrap";
+import PaymentForm from "./components/addCard";
 import MainNavbar from "./components/Navbar";
 
 const Deposit: NextPage = () => {
 	return (
 		<div>
-			<Container>Deposit</Container>
+			<Container>
+				Deposit
+				<PaymentForm></PaymentForm>
+			</Container>
 		</div>
 	);
 };
