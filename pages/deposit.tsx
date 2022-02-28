c37df9dea3a30702d2a17ea0de6725b95586b8a4
danielromeo <danielromeo99@gmail.com>
2022-02-28 21:11:47 +0200

    adding the transfer page HTML

--- pages/deposit.tsx
+++ pages/deposit.tsx
@@ -8,7 +8,7 @@ import {
 	Alert,
 	FormGroup,
 	Label,
-	Collapse
+	Collapse,
 } from "reactstrap";
 import { useForm } from "react-hook-form";
 
@@ -22,7 +22,7 @@ import AddedCardsSection from "./components/bankCards/AddedCardsSection";
 
 const PaymentForm = () => {
 	const { register, handleSubmit } = useForm();
-	    const [isOpen, setIsOpen] = React.useState(false);
+	const [isOpen, setIsOpen] = React.useState(false);
 
 	const onSubmit = (data: any) => {
 		Axios.post(`${process.env.REACT_APP_SERVER}/signup`, {
@@ -30,7 +30,7 @@ const PaymentForm = () => {
 			details: data.details,
 			month: data.month,
 			year: data.year,
-			cvc: data.cvc
+			cvc: data.cvc,
 		});
 		// .then((res)=>{
 		// 	console.log(res.data);
@@ -42,91 +42,77 @@ const PaymentForm = () => {
 		// })
 	};
 
-	return <div>
+	return (
+		<div>
+			<Button
+				color="primary"
+				onClick={() => {
+					setIsOpen(!isOpen);
+				}}
+			>
+				Add a payments card
+			</Button>
 
-		<Button 
-			color="primary" 
-			onClick={() => {
-					setIsOpen(!isOpen)
-				}
-			}>
-			Add a payments card
-		</Button>
+			<Collapse isOpen={isOpen}>
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<FormGroup>
+						<Label for="">Card number</Label>
+						<Input
+							style={cardnumberInputStyle}
+							{...register("cardnumber")}
+							type="text"
+							name="cardnumber"
+							id=""
+						/>
+					</FormGroup>
 
-		<Collapse isOpen={isOpen}>
-			<form onSubmit={handleSubmit(onSubmit)}>
-				<FormGroup>
-					<Label for="">
-						Card number
-					</Label>
-					<Input
-						style={cardnumberInputStyle}
-						{...register("cardnumber")}
-						type="text"
-						name="cardnumber"
-						id=""
-					/>
-				</FormGroup>
+					<FormGroup>
+						<Label for="">Card details</Label>
+						<Input
+							style={carddetailsInputStyle}
+							{...register("carddetails")}
+							type="text"
+							name="carddetails"
+							id=""
+						/>
+					</FormGroup>
 
-				<FormGroup>
-					<Label for="">
-						Card details
-					</Label>
-					<Input
-						style={carddetailsInputStyle}
-						{...register("carddetails")}
-						type="text"
-						name="carddetails"
-						id=""
-					/>
-				</FormGroup>
+					<FormGroup>
+						<Label for="">Expiry month</Label>
+						<Input
+							style={monthInputStyle}
+							{...register("month")}
+							type="text"
+							name="month"
+							id=""
+						/>
+					</FormGroup>
 
-				<FormGroup>
-					<Label for="">
-						Expiry month
-					</Label>
-					<Input
-						style={monthInputStyle}
-						{...register("month")}
-						type="text"
-						name="month"
-						id=""
-					/>
-				</FormGroup>
+					<FormGroup>
+						<Label for="">Expiry year</Label>
+						<Input
+							style={yearInputStyle}
+							{...register("year")}
+							type="text"
+							name="year"
+							id=""
+						/>
+					</FormGroup>
 
-				<FormGroup>
-					<Label for="">
-						Expiry year
-					</Label>
-					<Input
-						style={yearInputStyle}
-						{...register("year")}
-						type="text"
-						name="year"
-						id=""
-					/>
-				</FormGroup>
-
-				<FormGroup>
-					<Label for="">
-						CVC
-					</Label>
-					<Input
-						style={CVCInputStyle}
-						{...register("cvc")}
-						type="text"
-						name="cvc"
-						id=""
-					/>
-				</FormGroup>
-			</form>
-		</Collapse>
-
-
-		
-		
-	</div>
-
+					<FormGroup>
+						<Label for="">CVC</Label>
+						<Input
+							style={CVCInputStyle}
+							{...register("cvc")}
+							type="text"
+							name="cvc"
+							id=""
+						/>
+					</FormGroup>
+				</form>
+			</Collapse>
+		</div>
+	);
 };
 
 const Deposit: NextPage = () => {
--- pages/transfer.tsx
+++ pages/transfer.tsx
@@ -1,11 +1,99 @@
 import type { NextPage } from "next";
-import { Container, Button } from "reactstrap";
-import MainNavbar from "./components/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useForm } from "react-hook-form";
+
+import { AuthenticatedNavbar } from "./components/Navbar";
+import {
+	Container,
+	Input,
+	Row,
+	Col,
+	Alert,
+	FormGroup,
+	Label,
+	Collapse,
+	Button
+} from "reactstrap";
+
 
 const Transfer: NextPage = () => {
+	const { register, handleSubmit } = useForm();
+
+	const onSubmit = (data: any) => {
+		Axios.post(`${process.env.REACT_APP_SERVER}/signup`, {
+			cardnumber: data.cardnumber,
+			details: data.details,
+			month: data.month,
+			year: data.year,
+			cvc: data.cvc,
+		});
+	};
+
+
 	return (
 		<div>
-			<Container>Transfer</Container>
+			<AuthenticatedNavbar></AuthenticatedNavbar>
+			<Container>
+				<form onSubmit={handleSubmit(onSubmit)}>
+						
+						<Row>
+							{/*<FormGroup>
+								<Label for="">Card number</Label>
+								<Input
+									{...register("cardnumber")}
+									type="text"
+									name="cardnumber"
+									id=""
+								/>
+							</FormGroup>
+
+							<FormGroup>
+								<Label for="">Card details</Label>
+								<Input
+									{...register("carddetails")}
+									type="text"
+									name="carddetails"
+									id=""
+								/>
+							</FormGroup>
+
+							<Button>Continue</Button>*/}
+
+							<Col md='4'>
+								Your Randnote
+								<h4>Your Randnote balance</h4>
+								<h3>RR 400</h3>
+							</Col>
+
+							<Col md='4'>
+								TO
+							</Col>
+
+							<Col md='4'>
+								<input {...register("receiversAddress")}
+									type="text"
+									name="receiversAddress"
+									id="" placeholder="input recievers address">
+								</input>
+							</Col>
+						</Row>
+						<hr></hr>
+
+						<Row>
+							<h3>Amount</h3>
+							<input
+								{...register("amount")}
+									type="text"
+									name="amount"
+									 placeholder="RR 33"></input>
+						</Row>
+
+						<Row>
+							<small>*Disclaimer: Before transfering of funds, make sure that you have read our terms or agreement.</small>
+						</Row>
+
+					</form>
+			</Container>
 		</div>
 	);
 };
