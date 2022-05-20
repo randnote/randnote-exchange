2423b731b29507867776b5aa1c35f7b4f39113d0
danielromeo <danielromeo99@gmail.com>
2022-05-20 02:10:41 +0200

    added modal to make deposit

--- pages/transactions.tsx
+++ pages/transactions.tsx
@@ -1,62 +1,102 @@
 import type { NextPage } from "next";
+import {useState} from 'react'
 import { Container, Row, Col } from "reactstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { AuthenticatedNavbar } from "./components/Navbar";
-import { Tabs,Tab, Card } from "react-bootstrap";
+import { Tabs, Tab, Card, Modal, Button } from "react-bootstrap";
 // import Tabs from 'react-bootstrap/Tabs'
 
 const Transactions: NextPage = () => {
+	const [showModal, setShowModal] = useState(false);
+	const handleClose = () => setShowModal(false);
+	const handleShow = () => setShowModal(true);
+
 	return (
 		<div>
 			<AuthenticatedNavbar></AuthenticatedNavbar>
-			<br/>
+			<br />
 
 			<Container>
 				<Row>
-					<Col md='6'>
-						<Card style={{ width: '18rem' }}>
+					<Col md="6">
+						<Card style={{ width: "18rem" }}>
 							<Card.Body>
 								<Card.Title>ZAR balance</Card.Title>
+
+								<Card.Text>ZAR 123.00</Card.Text>
 								
-								<Card.Text>
-								ZAR 123.00
-								</Card.Text>
-								<Card.Link href="#">Make deposit</Card.Link>
-								
+								<Button variant="primary" onClick={handleShow}>
+									Make deposit
+									</Button>
 							</Card.Body>
 						</Card>
 					</Col>
 
-					<Col md='6'>
-						<Card style={{ width: '18rem' }}>
+					<Col md="6">
+						<Card style={{ width: "18rem" }}>
 							<Card.Body>
 								<Card.Title>Notes balance</Card.Title>
-								
-								<Card.Text>
-								N 29293.000023
-								</Card.Text>
+
+								<Card.Text>N 29293.000023</Card.Text>
 								<Card.Link href="#">Trade</Card.Link>
-								
 							</Card.Body>
 						</Card>
 					</Col>
 				</Row>
 			</Container>
 
-			<br/>
+			<br />
 
 			<Container>
-				<Tabs defaultActiveKey="profile" id="uncontrolled-tab-example" className="mb-3">
-					<Tab eventKey="websiteTransactions" title="Website Transactions">
+				<Tabs
+					defaultActiveKey="profile"
+					id="uncontrolled-tab-example"
+					className="mb-3"
+				>
+					<Tab
+						eventKey="websiteTransactions"
+						title="Website Transactions"
+					>
 						masesdfsdf
 					</Tab>
-					<Tab eventKey="blockchainTransactions" title="Blockchain Transactions">
+					<Tab
+						eventKey="blockchainTransactions"
+						title="Blockchain Transactions"
+					>
 						sdfsdf
 					</Tab>
-					
 				</Tabs>
 			</Container>
+
+				<Modal show={showModal} onHide={handleClose}>
+				<Modal.Header closeButton>
+					<Modal.Title>Modal heading</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+				<Modal.Footer>
+					<Button variant="secondary" onClick={handleClose}>
+					Close
+					</Button>
+					<Button variant="primary" onClick={handleClose}>
+					Save Changes
+					</Button>
+				</Modal.Footer>
+				</Modal>
 		</div>
 	);
 };
+
+function DepositModal() {
+	
+  
+	return (
+	  <>
+		
+  
+		
+	  </>
+	);
+  }
+
+
 export default Transactions;
