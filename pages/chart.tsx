fbc3a923a89a02d15a29d45d727e2aa7660099b2
danielromeo <danielromeo99@gmail.com>
2022-05-22 23:08:47 +0200

    chart works as should in chart.js

--- pages/chart.tsx
+++ pages/chart.tsx
@@ -1,13 +1,13 @@
 import type { NextPage } from "next";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Container, Button, Row, Col } from "reactstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { AuthenticatedNavbar } from "./components/Navbar";
 // import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import socketIOClient from "socket.io-client";
 const ENDPOINT = "http://127.0.0.1:8024";
-import { Line } from 'react-chartjs-2';
-import { faker } from '@faker-js/faker';  
+import { Line } from "react-chartjs-2";
+import { faker } from "@faker-js/faker";
 
 import {
 	Chart as ChartJS,
@@ -18,9 +18,9 @@ import {
 	Title,
 	Tooltip,
 	Legend,
-  } from 'chart.js';
+} from "chart.js";
 
-  ChartJS.register(
+ChartJS.register(
 	CategoryScale,
 	LinearScale,
 	PointElement,
@@ -28,67 +28,70 @@ import {
 	Title,
 	Tooltip,
 	Legend
-  );
+);
 //   const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
 
-
 let zaga = {
-	labels: ['January', 'February', 'March','April', 'May', 'June', 'July'],
+	labels: ["January", "February", "March", "April", "May", "June", "July"],
 	datasets: [
-	  {
-		label: 'Dataset 1',
-		data: [100,200,300,500],
-		borderColor: 'rgb(255, 99, 132)',
-		backgroundColor: 'rgba(255, 99, 132, 0.5)',
-	  },
+		{
+			label: "Dataset 1",
+			data: [100, 200, 300, 500],
+			borderColor: "rgb(255, 99, 132)",
+			backgroundColor: "rgba(255, 99, 132, 0.5)",
+		},
 	],
-  };
+};
 
-
-  // =-============================== main component
+// =-============================== main component
 const Chart: NextPage = () => {
 	const [response, setResponse] = useState({
-			labels: ['January', 'February', 'March','April', 'May', 'June', 'July'],
-			datasets: [
-			  {
-				label: 'Dataset 1',
-				data: [100,200,300,500],
-				borderColor: 'rgb(255, 99, 132)',
-				backgroundColor: 'rgba(255, 99, 132, 0.5)',
-			  },
-			],
-		  })
-
+		labels: [
+			"January",
+			"February",
+			"March",
+			"April",
+			"May",
+			"June",
+			"July",
+		],
+		datasets: [
+			{
+				label: "Dataset 1",
+				data: [100, 200, 300, 500],
+				borderColor: "rgb(255, 99, 132)",
+				backgroundColor: "rgba(255, 99, 132, 0.5)",
+			},
+		],
+	});
 
 	useEffect(() => {
 		const socket = socketIOClient(ENDPOINT);
-		
-		 
+
 		//   console.log(zaga)
 		socket.on("FromAPI", (data) => {
 			// setResponse(zaga)
-			let price : number = data;
+			let price: number = data;
 			let arrayLabels: any = response.labels;
 			let arrayPrices: any = response.datasets[0].data;
-			arrayLabels.push('new')
+			arrayLabels.push("new");
 			arrayPrices.push(price);
 
-
-			let newObj:any = {
+			let newObj: any = {
 				labels: arrayLabels,
 				datasets: [
-				  {
-					label: 'Dataset 1',
-					data: arrayPrices,
-					borderColor: 'rgb(255, 99, 132)',
-					backgroundColor: 'rgba(255, 99, 132, 0.5)',
-				  },
+					{
+						label: "Dataset 1",
+						data: arrayPrices,
+						borderColor: "rgb(255, 99, 132)",
+						backgroundColor: "rgba(255, 99, 132, 0.5)",
+					},
 				],
-			  };
+			};
 
-			  setResponse(newObj)
+			setResponse(newObj);
 
-			console.log(data)
+			console.log(data);
 
 			// setResponse(data);
 			// let vara : any = chartArray;
@@ -104,17 +107,15 @@ const Chart: NextPage = () => {
 	}, []);
 	return (
 		<div>
-			{
-				response ? <div>hall</div>: ''
-			}
+			{response ? <div>hall</div> : ""}
 			<AuthenticatedNavbar></AuthenticatedNavbar>
-			return <Line  data={response} />
+			return <Line data={response} />
 		</div>
 	);
 };
 
 const chartstyle = {
-	height: '500px'
+	height: "500px",
 };
 
 export default Chart;
