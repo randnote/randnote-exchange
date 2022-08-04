0a78e9c98dbcc3e3ed95b7029c55669d962e1fbc
danielromeo <danielromeo99@gmail.com>
2022-08-04 14:02:44 +0200

    finished navbar migration

--- pages/components/Navbar.tsx
+++ pages/components/Navbar.tsx
@@ -1,26 +1,13 @@
 import React, { useState, useEffect } from "react";
 import GetLocalStorage from "./authentication/localstorage";
 import Link from "next/link";
-// import {
-// 	Collapse,
-// 	Navbar,
-// 	NavbarToggler,
-// 	NavbarBrand,
-// 	Nav,
-// 	NavItem,
-// 	NavLink,
-// 	UncontrolledDropdown,
-// 	DropdownToggle,
-// 	DropdownMenu,
-// 	DropdownItem,
-// 	NavbarText,
-// 	Button,
-// } from "reactstrap";
 
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import {Button} from 'react-bootstrap';
 
 // style imports:
 import styles from "../../styles/Navbar.module.scss";
@@ -40,92 +27,13 @@ export const AuthenticatedNavbar = (props: any) => {
 		}
 	}, []);
 
-	// return (
-	// 	<div>
-	// 		<Navbar className={styles.authNavStyles} responsive>
-	// 			<NavbarBrand href="/">
-	// 				<b>RANDNOTE</b>
-	// 			</NavbarBrand>
-
-	// 			<NavbarToggler onClick={toggle} />
-
-	// 			<Collapse isOpen={isOpen} navbar>
-	// 				<Nav className="mr-auto" navbar>
-	// 					<NavItem>
-	// 						<NavLink
-	// 							style={{ color: "white" }}
-	// 							href="/dashboard/"
-	// 						>
-	// 							Dashboard
-	// 						</NavLink>
-	// 					</NavItem>
-	// 				</Nav>
-
-	// 				<Nav className="mr-auto" navbar>
-	// 					<NavItem>
-	// 						<NavLink
-	// 							style={{ color: "white" }}
-	// 							href="/deposit/"
-	// 						>
-	// 							Deposits
-	// 						</NavLink>
-	// 					</NavItem>
-	// 				</Nav>
-
-	// 				<Nav className="mr-auto" navbar>
-	// 					<NavItem>
-	// 						<NavLink
-	// 							style={{ color: "white" }}
-	// 							href="/transactions/"
-	// 						>
-	// 							Transactions
-	// 						</NavLink>
-	// 					</NavItem>
-	// 				</Nav>
-
-	// 				<Nav className="mr-auto" navbar>
-	// 					<NavItem>
-	// 						<NavLink style={{ color: "white" }} href="/chart/">
-	// 							Chart
-	// 						</NavLink>
-	// 					</NavItem>
-	// 				</Nav>
-
-	// 				{isAuthenticated ? (
-	// 					""
-	// 				) : (
-	// 					<Nav className="navbar-nav ms-auto">
-	// 						<NavItem className="ms-auto">
-	// 							<Link href="signin">
-	// 								<button
-	// 									className="btn "
-	// 									// styles={AuthNavbarButtonStyles}
-	// 									color=""
-	// 								>
-	// 									SignIn
-	// 								</button>
-	// 							</Link>
-
-	// 							<Link href="signup">
-	// 								<Button
-	// 									styles={AuthNavbarButtonStyles}
-	// 									color="primary"
-	// 								>
-	// 									SignUp
-	// 								</Button>
-	// 							</Link>
-	// 						</NavItem>
-	// 					</Nav>
-	// 				)}
-	// 			</Collapse>
-	// 		</Navbar>
-	// 	</div>
-	// );
 
 	return (
 		<Navbar bg="light" expand="lg">
 			<Container>
-				<Navbar.Brand href="/"><b>RANDNOTE</b></Navbar.Brand>
+				<Navbar.Brand href="/">
+					<b>RANDNOTE</b>
+				</Navbar.Brand>
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 
 				<Navbar.Collapse id="basic-navbar-nav">
@@ -133,22 +41,37 @@ export const AuthenticatedNavbar = (props: any) => {
 						<Nav.Link href="/transactions">Transactions</Nav.Link>
 						<Nav.Link href="/deposit">Deposit</Nav.Link>
 						<Nav.Link href="/chart">Chart</Nav.Link>
-
-						{/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-						<NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-						<NavDropdown.Item href="#action/3.2">
-							Another action
-						</NavDropdown.Item>
-						<NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-						<NavDropdown.Divider />
-						<NavDropdown.Item href="#action/3.4">
-							Separated link
-						</NavDropdown.Item>
-						</NavDropdown> */}
 					</Nav>
+
+					{isAuthenticated ? (
+						""
+					) : (
+						<Nav className="navbar-nav ms-auto">
+							<Nav.Link className="ms-auto">
+								<Link href="signin">
+									<button
+										className="btn "
+										// styles={AuthNavbarButtonStyles}
+										color=""
+									>
+										SignIn
+									</button>
+								</Link>
+
+								<Link href="signup">
+									<Button
+										style={AuthNavbarButtonStyles}
+										color="primary"
+									>
+										SignUp
+									</Button>
+								</Link>
+							</Nav.Link>
+						</Nav>
+					)}
 				</Navbar.Collapse>
 			</Container>
-    	</Navbar>
+		</Navbar>
 	);
 };
 
