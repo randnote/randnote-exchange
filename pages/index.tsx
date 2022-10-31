99632b104bd7757da779ecd136b1bb2b7fa00b48
danielromeo <danielromeo99@gmail.com>
2022-10-31 19:54:18 +0200

    worked on about section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
 import type { NextPage } from "next";
 import { AuthenticatedNavbar } from "./components/Navbar";
 import MainFooter from "./components/Footer";
-import { Container, Button, Row } from "reactstrap";
+import { Container, Button, Row, Card, CardBody,CardTitle ,CardText} from "reactstrap";
 import ChartComponent from "./components/charts/chartComponent";
+import Image from 'next/image'
+import MainImage from '../public/blocks.png'
 
+import {GrMoney} from 'react-icons/gr'
+import {GiCycle} from 'react-icons/gi'
+import {SiHiveBlockchain} from 'react-icons/si'
 // styles imports:
 import styles from "../styles/Home.module.scss";
 
@@ -16,9 +21,24 @@ const MainComponent: React.FC = () => {
 	return (
 		<div className={styles.mainComponent}>
 			<Container className={styles.mainComponentMainContainer}>
-				<Row className={styles.mainRow}>
-					<h1>Rand Note Exchange</h1>
-					<p>This is the RandNote official website and exchange.</p>
+				<Row className={`${styles.mainRow} `}>
+					
+					<div className={`${styles.colLeft} col-md-6 col-sm-12`}>
+						<h1>Rand Note Exchange</h1>
+						<p>This is the RandNote official website and exchange.</p>
+					</div>
+
+					<div className={`${styles.colRight} col-md-6 col-sm-12`}>
+						<Image
+							src={MainImage}
+							alt="Picture of the author"
+							width={600}
+							height={500} 
+							// blurDataURL="data:..." automatically provided
+							// placeholder="blur" // Optional blur-up while loading
+						/>
+					</div>
+
 				</Row>
 			</Container>
 		</div>
@@ -28,29 +48,59 @@ const MainComponent: React.FC = () => {
 // About section
 const AboutComponent = () => {
 	return (
-		<div style={AboutComponentDiv}>
+		<div className={styles.aboutComponent}>
 			<Container>
 				<div className="text-center">
+					
 					<Row>
-						<p className="text-center">
-							Randnote is lorem
-							ipsum...............................h1
-						</p>
+						<h3 className={`${styles.infoSection}`}>
+							One blockchain to simulate all your needs!
+						</h3>
 					</Row>
 
-					<Row>
-						<p className="text-center">
-							<Button
-								style={{
-									color: "white",
-									backgroundColor: "green",
-									borderRadius: "20px",
-									maxWidth: "300px",
-								}}
-							>
-								WHITEPAPER
-							</Button>
-						</p>
+					<Row className={styles.blockSection}>
+						<div className="col-md-4">
+							<Card>
+								<CardBody>
+									<GrMoney size={50}></GrMoney>
+
+									<CardTitle tag="h5">
+									Money 
+									</CardTitle>
+									<CardText>
+									This application simulates real world money transactions such as depositing and withdrawing funds, using those funds to trade the cryptocurrency. 
+									</CardText>
+								</CardBody>	
+							</Card>
+						</div>
+
+						<div className="col-md-4">
+							<Card>
+								<CardBody>
+									<SiHiveBlockchain size={50}></SiHiveBlockchain>
+									<CardTitle tag="h5">
+									Blockchain simulation
+									</CardTitle>
+									<CardText>
+									This application consists of a blockchain written in Typescript(Node) that runs in the backend.
+									</CardText>
+								</CardBody>	
+							</Card>
+						</div>
+
+						<div className="col-md-4">
+							<Card>
+								<CardBody>
+									<GiCycle size={40}></GiCycle>
+									<CardTitle tag="h5">
+									Blockchain automation
+									</CardTitle>
+									<CardText>
+									This application consists of an automator application that is responsible for automating user transactions(buys, sells and transfers).
+									</CardText>
+								</CardBody>	
+							</Card>
+						</div>
 					</Row>
 				</div>
 			</Container>
@@ -63,7 +113,7 @@ const Home: NextPage = () => {
 	return (
 		<div>
 			<AuthenticatedNavbar></AuthenticatedNavbar>
-
+			<MainComponent></MainComponent>
 			<AboutComponent></AboutComponent>
 			<MainFooter></MainFooter>
 		</div>
